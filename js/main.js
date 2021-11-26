a713a73aab0f67302fc42db56541399ecb28058a
SM Boolean <samuelemadrigali1991@gmail.com>
2021-11-26 10:29:19 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue({
+	el: '#root',
+	data: {
+		messaggio: "Quanto è bello Vuejs!!",
+		img: "img/vuejs-logo.jpg"
+	}
+});
